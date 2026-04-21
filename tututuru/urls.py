7fae41fcd561340243761bc433129f49df3d1c15
Isaque Dias <isaque.brain@gmail.com>
2026-04-21 20:59:30 -0300

    first commit

---
+++ tututuru/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import IndexView, ContatoView
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('contato/', ContatoView.as_view(), name='contato'),
+]
